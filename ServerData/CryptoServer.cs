b10b60141897533abeecfbf459faa94ab084711c
Arne (USA) <aleitert@kent.edu>
2019-12-10 16:51:16 -0800

    Added ServerBase and CryptoServer classes.

---
+++ ServerData/CryptoServer.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Sockets;
+
+namespace ServerData
+{
+    public class CryptoServer : ServerBase
+    {
+        protected override ClientBase CreateClient(Socket socket)
+        {
+            return new CryptoClient(socket);
+        }
+    }
+}
